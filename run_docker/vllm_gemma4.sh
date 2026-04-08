40d4fe1a3c1354616dcdf16ac414c237614d27a7
phonexue <phonexue@hotmail.com>
2026-04-08 23:57:05 +0800

    优化 vllm-gemma4的启动参数

--- run_docker/vllm_gemma4.sh
+++ run_docker/vllm_gemma4.sh
@@ -6,12 +6,26 @@ docker run --rm -it \
   -v $HOME/models/gemma4-31b:/workspace/ \
   -p 8000:8000 \
   vllm/vllm-openai:v0.18.1-aarch64-cu130 \
-  python3 -m vllm.entrypoints.openai.api_server \
     --model /workspace \
     --served-model-name gemma4 \
     --host 0.0.0.0 \
     --port 8000 \
+    --quantization fp8 \
     --dtype auto \
+    --max-model-len 4096 \
+    --max-num-batched-tokens 512 \
+    --max-num-seqs 8 \
+    --enable-chunked-prefill \
+    --enable-prefix-caching \
+    --gpu-memory-utilization 0.9 \
+    --scheduling-policy fcfs \
+    --num-scheduler-steps 1 \
+    --trust-remote-code \
     --tensor-parallel-size 1 \
-    --max-model-len 128000 \
-    --gpu-memory-utilization 0.90 
+    --api-key free-key \
+    --enforce-eager \
+    --enable-auto-tool-choice \
+    --tool-call-parser gemma4 \
+    --enable-reasoning \
+    --reasoning-parser gemma4 
+
