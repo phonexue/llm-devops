73da423033233a52b6b559f62368a3508ee73480
phonexue <phonexue@hotmail.com>
2026-04-09 07:58:38 +0800

    modify the compose paraments

--- run_docker/vllm_gemma4.sh
+++ run_docker/vllm_gemma4.sh
@@ -3,7 +3,7 @@ docker run --rm -it \
   --name gemma-serve \
   --gpus all \
   --ipc=host \
-  -v $HOME/models/gemma4-26b:/workspace/ \
+  -v $HOME/models/gemma4-26b:/workspace \
   -p 8000:8000 \
   vllm/vllm-openai:gemma4-cu130 \
     --model /workspace \
@@ -18,14 +18,11 @@ docker run --rm -it \
     --enable-chunked-prefill \
     --enable-prefix-caching \
     --gpu-memory-utilization 0.9 \
-    --scheduling-policy fcfs \
-    --num-scheduler-steps 1 \
     --trust-remote-code \
     --tensor-parallel-size 1 \
     --api-key free-key \
     --enforce-eager \
     --enable-auto-tool-choice \
     --tool-call-parser gemma4 \
-    --enable-reasoning \
     --reasoning-parser gemma4 
 
