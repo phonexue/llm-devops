39b4fa401f029c075196359ed8b69d86c3e0afe1
phonexue <phonexue@hotmail.com>
2026-04-21 21:33:11 +0800

    修改优化 qwen3.6 的 vllm 推理参数

--- run_docker/vllm_qwen3.6.sh
+++ run_docker/vllm_qwen3.6.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 docker run -d \
-  --name gemma4-server \
+  --name qwen3.6-server \
   --gpus all \
   --ipc=host \
   --restart=unless-stopped \
-  -v $HOME/models/gemma4-26b:/workspace \
-  -p 8000:8000 \
-  vllm/vllm-openai:gemma4-cu130 \
-    --model /workspace \
-    --served-model-name gemma4 \
+  -v $HOME/models/qwen3.6-35b:/workspace \
+  -p 8001:8001 \
+  vllm/vllm-openai:v0.19.1-aarch64-cu130 \
+    /workspace \
+    --served-model-name qwen3.6 \
     --host 0.0.0.0 \
-    --port 8000 \
+    --port 8001 \
     --dtype auto \
     --max-model-len 128000 \
-    --max-num-batched-tokens 1024 \
+    --max-num-batched-tokens 2048 \
     --max-num-seqs 8 \
     --enable-chunked-prefill \
     --enable-prefix-caching \
-    --gpu-memory-utilization 0.45 \
+    --gpu-memory-utilization 0.8 \
     --trust-remote-code \
     --tensor-parallel-size 1 \
     --api-key free-key \
     --enforce-eager \
     --enable-auto-tool-choice \
-    --tool-call-parser gemma4 \
-    --reasoning-parser gemma4 
-
+    --tool-call-parser qwen3_coder \
+    --reasoning-parser qwen3 
