e8d890ba458767d2ae29f049a650f05bd628c2a6
phonexue <phonexue@hotmail.com>
2026-05-03 16:48:19 +0800

    增加hermes 的 it agent， 增加 vllm 脚本支持 qwen3.6-27b 模型

---
+++ run_docker/vllm_qwen36-27b.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+docker run -d \
+  --name qwen36-server \
+  --gpus all \
+  --ipc=host \
+  --restart=unless-stopped \
+  -v $HOME/models/qwen3.6-27b:/workspace \
+  -p 8001:8001 \
+  vllm/vllm-openai:v0.19.1-aarch64-cu130 \
+    /workspace \
+    --served-model-name qwen3.6 \
+    --host 0.0.0.0 \
+    --port 8001 \
+    --dtype auto \
+    --max-model-len 128000 \
+    --max-num-batched-tokens 2048 \
+    --max-num-seqs 8 \
+    --enable-chunked-prefill \
+    --enable-prefix-caching \
+    --gpu-memory-utilization 0.9 \
+    --trust-remote-code \
+    --tensor-parallel-size 1 \
+    --api-key free-key \
+    --enforce-eager \
+    --enable-auto-tool-choice \
+    --tool-call-parser qwen3_coder \
+    --reasoning-parser qwen3 
